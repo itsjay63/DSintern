1be2d69a7081987985602dc21becbd5b25371f3d
jay <jaysuthar0603@gmail.com>
2021-09-05 17:29:41 +0530

    day-2 code and note added

---
+++ day-2 python basic-1/Day_002.py
@@ -0,0 +1,58 @@
+print ("Hello Guys!") #Hello Guys!
+print (2+3) #5
+#windows - use F9 key for execution of the selected line
+
+""" learning flow 
+data types
+input from the User
+control flow - if else elif
+looping - while for
+string operations
+internal datastructure - list, dict, tuple..
+file handling
+"""
+
+a = 10
+b =  2.3
+c = True
+d = "Forsk"
+e = 'Forsk'
+f = "F"
+g = 'F'
+h = None #null,same as c and c++
+
+""" data types 
+int
+float
+bool
+str
+NoneType
+"""
+
+x  = "10"
+cname = "Forsk"
+print (type(cname)) #<str>
+len(cname) #5
+print (cname) #Forsk
+
+cname[0] #F
+cname[1] #o
+cname[-1] #k
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
